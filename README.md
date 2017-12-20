af887f8c6117bb1b9f3a516f8068feb7b96c7c0d
yasacurry <yasacurry@gmail.com>
2017-12-21 01:07:38 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# mill
+
+yasacurry/harvest で保存したファイルの分かち書き、モデル保存、対話用スクリプト
+
+## usage 
+`$ python wakati.py data.csv`  
+`$ python model.py data.csv.wakati`  
+`$ python -i interactive.py data.csv.wakati.model`
+
---
+++ wakati.py
@@ -0,0 +1,39 @@
+import sys
+import re
+import pandas
+import MeCab
+
+csv_name = sys.argv[1]
+wakati_name = csv_name + ".wakati"
+
+csv_header = ('created_at', 'service_name', 'source_id', 'source_url',
+              'user_id', 'user_name', 'text', 'shared_by', 'csv_write_at')
+data_frame = pandas.read_csv(csv_name, encoding="utf-8", names=csv_header)
+
+parts = ["名詞", "動詞", "形容詞", "副詞", "未知語"]
+
+# URL, hashtags, screen_name
+regex = r"https?://[a-zA-Z0-9\.\-_/]+|#.+?|@[a-zA-Z0-9_]+"
+
+tagger = MeCab.Tagger("-Ochasen")
+# https://qiita.com/kasajei/items/0805b433f363f1dba785 と同じ症状になったので一回parseをする
+tagger.parse("")
+
+writer = open(wakati_name, "w")
+
+for text in data_frame["text"]:
+    newtext = re.sub(regex, "", text)
+    node = tagger.parseToNode(newtext)
+
+    while node:
+        feature = node.feature.split(",")
+        if feature[0] in parts:
+            if feature[6] == "*":  # 未知語はそのまま出したい
+                writer.write(node.surface + " ")
+            else:
+                writer.write(feature[6] + " ")
+        node = node.next
+    writer.write("\n")
+
+writer.close()
+
