3e4fbb3406a3a7ba945205fda61fba65aa0d4ba5
yasacurry <yasacurry@gmail.com>
2018-01-05 19:45:08 +0900

    add wakati.py -s option
    表層形出力のために -s, --surface オプションを追加

--- wakati.py
+++ wakati.py
@@ -1,14 +1,21 @@
 import sys
 import re
+import argparse
 import pandas
 import MeCab
 
-csv_name = sys.argv[1]
-wakati_name = csv_name + ".wakati"
+parser = argparse.ArgumentParser()
+parser.add_argument("csv", help="target file (github.com/yasacurry/harvest)")
+parser.add_argument(
+    "-s",
+    "--surface",
+    help="use MeCab-chasen type surface",
+    action="store_true")
+args = parser.parse_args()
 
 csv_header = ('created_at', 'service_name', 'source_id', 'source_url',
               'user_id', 'user_name', 'text', 'shared_by', 'csv_write_at')
-data_frame = pandas.read_csv(csv_name, encoding="utf-8", names=csv_header)
+data_frame = pandas.read_csv(args.csv, encoding="utf-8", names=csv_header)
 
 parts = ["名詞", "動詞", "形容詞", "副詞", "未知語"]
 
@@ -19,7 +26,7 @@ tagger = MeCab.Tagger("-Ochasen")
 # https://qiita.com/kasajei/items/0805b433f363f1dba785 と同じ症状になったので一回parseをする
 tagger.parse("")
 
-writer = open(wakati_name, "w")
+writer = open(args.csv + ".wakati", "w")
 
 for text in data_frame["text"]:
     newtext = re.sub(regex, " ", text)
@@ -27,13 +34,15 @@ for text in data_frame["text"]:
 
     while node:
         feature = node.feature.split(",")
-        if feature[0] in parts:
-            if feature[6] == "*":  # 未知語はそのまま出したい
-                writer.write(node.surface + " ")
-            else:
-                writer.write(feature[6] + " ")
+        if args.surface:
+            writer.write(node.surface + " ")
+        else:
+            if feature[0] in parts:
+                if feature[6] == "*":  # 未知語はそのまま出したい
+                    writer.write(node.surface + " ")
+                else:
+                    writer.write(feature[6] + " ")
         node = node.next
     writer.write("\n")
 
 writer.close()
-
